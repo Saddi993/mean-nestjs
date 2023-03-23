7bb2806611392270b57c19ffa281ab794f41d63b
Saad Ahmed <saad.ahmed@teamingway.com>
2023-03-23 11:50:39 +0500

    added user register api and emit date to user micro service

---
+++ gateway-service/src/users/users.controller.ts
@@ -0,0 +1,15 @@
+import { Body, Controller, Post } from "@nestjs/common";
+import { User } from "./users.model";
+import { UserService } from "./users.service";
+
+@Controller('users')
+export class Users {
+
+    constructor(private readonly userService: UserService) { }
+
+    @Post()
+    register(@Body() createUserReq: User) {
+        const userId = this.userService.createUser(createUserReq);
+        return { userId };
+    }
+}
---
+++ gateway-service/src/users/users.service.ts
@@ -0,0 +1,14 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
+import { User } from './users.model';
+
+@Injectable()
+export class UserService {
+
+    constructor (@Inject('USER_MICROSERVICE') private readonly userMicroServiceClient: ClientProxy) {}
+
+    createUser(createUserReq: User) {
+        console.log(createUserReq);
+        this.userMicroServiceClient.emit('user_register', createUserReq)
+    }
+}
