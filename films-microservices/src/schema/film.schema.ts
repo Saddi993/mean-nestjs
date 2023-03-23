2dce31f8f238009400df2598cbcf36918cf83119
Saad Ahmed <saad.ahmed@teamingway.com>
2023-03-23 22:44:43 +0500

    fix data type

--- films-microservices/src/schema/film.schema.ts
+++ films-microservices/src/schema/film.schema.ts
@@ -3,7 +3,7 @@ import * as mongoose from 'mongoose';
 export const FilmSchema = new mongoose.Schema({
   title: { type: String, required: true },
   director: { type: String, required: true },
-  year: { type: String, required: true },
+  year: { type: Number, required: true },
   actors: { type: Array, required: true },
 });
 
