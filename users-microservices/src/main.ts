ac85725c5c1aafcfdf4fac528cd0c8815cdd5e29
Saad Ahmed <saad.ahmed@teamingway.com>
2023-03-23 12:49:14 +0500

    fixed port in use issue

--- users-microservices/src/main.ts
+++ users-microservices/src/main.ts
@@ -3,7 +3,14 @@ import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, { transport: Transport.TCP });
-  await app.listen();
+  const app = await NestFactory.create(AppModule);
+  app.connectMicroservice({
+    transport: Transport.TCP,
+    options: {
+      port: 3001,
+    },
+  });
+  await app.startAllMicroservices();
+  // await app.listen(3001);
 }
 bootstrap();
