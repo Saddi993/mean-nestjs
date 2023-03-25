9cb7c814dede976fce46a28cb0ad3629f16da0ab
Saad Ahmed <saad.ahmed@teamingway.com>
2023-03-25 19:52:36 +0500

    remvoed unused routes

--- angular-src/src/app/app-routing.module.ts
+++ angular-src/src/app/app-routing.module.ts
@@ -2,17 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
-import { ProductDetailComponent } from './pages/product-detail/product-detail.component';
-import { ProductListComponent } from './pages/product-list/product-list.component';
 import { RegisterComponent } from './pages/register/register.component';
 
 
 const routes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'home', component: HomeComponent },
-  { path: 'products', component: ProductListComponent },
-  { path: 'product-detail/:id', component: ProductDetailComponent },
+  { path: 'home', component: HomeComponent }
 ];
 
 @NgModule({
